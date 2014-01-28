cfc448dec9161e1943fa5169ac18b4d051c48e1f
Camillus Gerard Cai <cgcai@qxcg.net>
2014-01-29 03:13:41 +0800

    Unhardened implementation of LsTool.

--- src/sg/edu/nus/comp/cs4218/impl/fileutils/LsTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/fileutils/LsTool.java
@@ -4,6 +4,8 @@ import sg.edu.nus.comp.cs4218.fileutils.ILsTool;
 import sg.edu.nus.comp.cs4218.impl.ATool;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -34,16 +36,55 @@ public class LsTool extends ATool implements ILsTool {
      */
     @Override
     public String execute(File workingDir, String stdin) {
-        return null;
+        if (this.args.length < 1) {
+            return listDirectory(workingDir);
+        } else {
+            File specified = new File(this.args[0]);
+            return listDirectory(specified);
+        }
     }
 
     @Override
     public List<File> getFiles(File directory) {
-        return null;
+        File[] files = directory.listFiles();
+        if (files == null) {
+            return new ArrayList<File>(); // Return empty array.
+        } else {
+            return Arrays.asList(files);
+        }
     }
 
     @Override
     public String getStringForFiles(List<File> files) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        for (File f : files) {
+            sb.append(String.format("%s\n", f.getName()));
+        }
+        return sb.toString();
+    }
+
+    private String listDirectory(File wd) {
+        if (wd == null) {
+            return null;
+        }
+
+        if (!validDirectory(wd)) {
+            return null;
+        }
+
+        return getStringForFiles(getFiles(wd));
+    }
+
+    private boolean validDirectory(File directory) {
+        // TODO: check whether given directory is valid.
+        return false;
+    }
+
+    private void statusError() {
+        this.setStatusCode(1);
+    }
+
+    private void statusSuccess() {
+        this.setStatusCode(0);
     }
 }
