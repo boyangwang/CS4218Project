1146f2cfca9ed49d97d8ac0c69a1aa9f11555f0a
Camillus Gerard Cai <cgcai@qxcg.net>
2014-02-04 21:10:35 +0800

    Unfinished CatTool.

--- src/sg/edu/nus/comp/cs4218/impl/fileutils/CatTool.java
+++ src/sg/edu/nus/comp/cs4218/impl/fileutils/CatTool.java
@@ -1,6 +1,5 @@
 package sg.edu.nus.comp.cs4218.impl.fileutils;
 
-import sg.edu.nus.comp.cs4218.IShell;
 import sg.edu.nus.comp.cs4218.fileutils.ICatTool;
 import sg.edu.nus.comp.cs4218.impl.ATool;
 
@@ -12,30 +11,25 @@ public class CatTool extends ATool implements ICatTool {
      * Constructor
      *
      * @param arguments Arguments the tool is going to be executed with.
-     * @param stdin
      */
-    public CatTool(String[] arguments, String stdin) {
-        super(arguments, stdin);
+    public CatTool(String[] arguments) {
+        super(arguments);
+    }
+
+    @Override
+    public String getStringForFile(File toRead) {
+        return null;
     }
 
     /**
      * Executes the tool with args provided in the constructor
      *
-     * @param shell
-     * @param workingDir The current working directory.
+     * @param workingDir
+     * @param stdin      Input on stdin. NOT THE ARGUMENTS! Can be null.
      * @return Output on stdout
      */
     @Override
-    public String execute(IShell shell, File workingDir) {
-        if (Thread.interrupted()) {
-            return null;
-        }
-
-        return null;
-    }
-
-    @Override
-    public String getStringForFile(File toRead) {
+    public String execute(File workingDir, String stdin) {
         return null;
     }
 }
