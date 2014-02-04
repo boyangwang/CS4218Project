83dc59e0c3e3b1a33fac140cd9adbd210b9d304c
Camillus Gerard Cai <cgcai@qxcg.net>
2014-02-04 20:13:20 +0800

    Extracted command parsing into static class.

---
+++ src/sg/edu/nus/comp/cs4218/impl/CommandParser.java
@@ -0,0 +1,83 @@
+package sg.edu.nus.comp.cs4218.impl;
+
+
+import sg.edu.nus.comp.cs4218.ITool;
+import sg.edu.nus.comp.cs4218.extended1.IPipingTool;
+import sg.edu.nus.comp.cs4218.impl.extended1.PipingTool;
+
+import java.util.Arrays;
+
+public class CommandParser {
+    public static ITool parse(String str) {
+        // at the beginning of Shell.parse, if pipe operator is present, pass to PipingTool
+        if (containsPipeOperator(str)) {
+            String[] args = new String[1];
+            args[0] = str;
+            IPipingTool pipingTool = new PipingTool(args, "");
+            return pipingTool;
+        }
+
+        String[] tokens = tokenizeString(str);
+        String[] argList = getArgumentList(tokens);
+        String cmd = getCommand(tokens);
+
+        switch (cmd) {
+            case "cat":
+                break;
+
+            case "cd":
+                break;
+
+            case "copy":
+                break;
+
+            case "delete":
+                break;
+
+            case "echo":
+                break;
+
+            case "ls":
+                break;
+
+            case "move":
+                break;
+
+            case "pwd":
+                break;
+
+            default:
+                Logging.logger(System.out).writeLog(Logging.Error, "Cannot parse " + str);
+                return null;
+        }
+
+        return null;
+    }
+
+    /**
+     * Parse inputString and returns true if the control should be
+     * passed to PipingTool
+     *
+     * @param str the user input string
+     * @return `true` if the control should be passed to PipingTool
+     */
+    private static boolean containsPipeOperator(String str) {
+        return str.contains("|");
+    }
+
+    private static String[] tokenizeString(String str) {
+        return str.split(" ");
+    }
+
+    private static String[] getArgumentList(String[] tokens) {
+        return Arrays.copyOfRange(tokens, 0, tokens.length - 2);
+    }
+
+    private static String getCommand(String[] tokens) {
+        if (tokens.length > 0) {
+            return tokens[0];
+        } else {
+            return "";
+        }
+    }
+}
