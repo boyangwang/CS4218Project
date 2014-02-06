664a26acdd9201917fece5d5be981ed5e567cae4
Navie Huynh <quahuynh@gmail.com>
2014-02-06 18:29:35 +0800

    default status to -1

--- test/sg/edu/nus/comp/cs4218/impl/fileutils/ShellTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/fileutils/ShellTest.java
@@ -15,7 +15,31 @@ import sg.edu.nus.comp.cs4218.fileutils.IEchoTool;
 import sg.edu.nus.comp.cs4218.impl.Shell;
 
 public class ShellTest {
+	private class TestInterruptTool implements ITool{
+		int _status;
+		public TestInterruptTool(){
+			_status = 1;
+		}
+		@Override
+		public String execute(File workingDir, String stdin) {
+			try {
+				System.out.println("Sleeping");
+				Thread.sleep(10*1000); //sleep 10 sec
+			} catch (InterruptedException e) {
+				_status = 0;
+				return null;
+			}
+			_status = 0;
+			return null;
+		}
 
+		@Override
+		public int getStatusCode() {
+			// TODO Auto-generated method stub
+			return _status;
+		}
+		
+	}
 	IShell sh;
 	@Before
 	public void setUp() throws Exception {
@@ -42,25 +66,25 @@ public class ShellTest {
 	public void parseCommandWrongCase() {
 		String cmd = "eCho you shall not pass";
 		ITool result = sh.parse(cmd);
-		assertEquals(result,null);
+		assertEquals(null,result);
 	}
 	@Test
 	public void parseCommandWrongQuote() {
 		String cmd = "echo 'you shall not pass ";
 		ITool result = sh.parse(cmd);
-		assertEquals(result, null);
+		assertEquals(null,result);
 	}
 	@Test
 	public void parseCommandWrongDQuote() {
 		String cmd = "echo \"you shall not pass ";
 		ITool result = sh.parse(cmd);
-		assertEquals(result, null);
+		assertEquals(null,result);
 	}
 	@Test
 	public void parseCommandWrongBQuote() {
 		String cmd = "echo `you shall not pass ";
 		ITool result = sh.parse(cmd);
-		assertEquals(result, null);
+		assertEquals(null,result);
 	}
 	@Test
 	public void parseCommandPipe(){
@@ -82,22 +106,31 @@ public class ShellTest {
 	}
 	@Test 
 	public void executeNormalCommand(){
+		sh = new Shell();
 		ITool tool = sh.parse("echo you shall not pass");
 		Runnable executor = sh.execute(tool);
-		assertNotEquals(executor,null);
+		assertNotEquals(null,executor);
 	}
 	@Test 
 	public void executeNull(){
+		sh = new Shell();
 		Runnable executor = sh.execute(null);
-		assertEquals(executor,null);
+		assertEquals(null,executor);
 	}
 	@Test
 	public void stopThread(){
-		ITool tool = sh.parse("echo you shall not pass");
+		sh = new Shell();
+		ITool tool = new TestInterruptTool();
+
 		Runnable executor = sh.execute(tool);
-		assertNotEquals(executor,null);
 		sh.stop(executor);
-		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		assertEquals(0,tool.getStatusCode());
 	}
 	
 }
