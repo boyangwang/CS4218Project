8ba091e7a824f0da88ce97506d5d12859038bc6d
boyang <wangboyangjinghua@gmail.com>
2014-03-04 14:51:30 +0800

    extended2

---
+++ src/sg/edu/nus/comp/cs4218/impl/extended2/CommTool.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.comp.cs4218.impl.extended2;
+
+import java.io.File;
+
+import sg.edu.nus.comp.cs4218.ITool;
+
+public class CommTool extends sg.edu.nus.comp.cs4218.impl.ATool implements ITool {
+
+	public CommTool(String[] arguments) {
+		super(arguments);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	/**
+	 * @param workingDir
+	 * @param stdin
+	 */
+	public String execute(File workingDir, String stdin) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
