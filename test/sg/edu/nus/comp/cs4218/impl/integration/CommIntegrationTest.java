1d8f35745d6eabb56ba31f42d6f546f421ddc771
boyang <wangboyangjinghua@gmail.com>
2014-03-16 22:50:51 +0800

    comm pipe it

---
+++ test/sg/edu/nus/comp/cs4218/impl/integration/CommIntegrationTest.java
@@ -0,0 +1,27 @@
+package sg.edu.nus.comp.cs4218.impl.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import sg.edu.nus.comp.cs4218.extended2.ICommTool;
+
+public class CommIntegrationTest {
+	ICommTool commTool;
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testCommCutPipe() {
+		
+	}
+
+}
---
+++ test/sg/edu/nus/comp/cs4218/impl/integration/PipeIntegrationTest.java
@@ -0,0 +1,24 @@
+package sg.edu.nus.comp.cs4218.impl.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class PipeIntegrationTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
