dee41e49b9e742f50765b561e384c3c2e11e4ce5
Camillus Gerard Cai <cgcai@qxcg.net>
2014-03-16 22:51:50 +0800

    Dev docs.

--- test/sg/edu/nus/comp/cs4218/impl/integration/UniqIntegrationTest.java
+++ test/sg/edu/nus/comp/cs4218/impl/integration/UniqIntegrationTest.java
@@ -11,15 +11,18 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.Assert.assertEquals;
 
 public class UniqIntegrationTest {
-    private final String TEST_INPUT_FILE = "uniq-test-in.txt";
-    private final String TEST_OUTPUT_FILE = "uniq-test-output.txt";
-
+    // Used to abstract Shell testing. You'll need these in a new suite.
     private ByteArrayInputStream simIn;
     private ByteArrayOutputStream simOut;
     private Shell shell;
+
+    // Used only in this test suite.
+    private final String TEST_INPUT_FILE = "uniq-test-in.txt";
+    private final String TEST_OUTPUT_FILE = "uniq-test-output.txt";
     private File fin;
     private File fout;
 
+    // You need this method.
     private String getStringFromOutput() {
         String out = new String(simOut.toByteArray(), StandardCharsets.UTF_8);
         out = out.substring(out.indexOf("> ") + 2).trim();
@@ -27,6 +30,7 @@ public class UniqIntegrationTest {
         return out;
     }
 
+    // You need this method.
     private void setupShellWithInput(String input) {
         // Terminate at the end of this command.
         input += "\r\nctrl-c\r\n";
@@ -40,8 +44,10 @@ public class UniqIntegrationTest {
      */
     @Before
     public void setUp() throws Exception {
+        // You need this line.
         simOut = new ByteArrayOutputStream();
 
+        // Only for this test suite/
         fin = new File(TEST_INPUT_FILE);
         fout = new File(TEST_OUTPUT_FILE);
     }
@@ -51,10 +57,10 @@ public class UniqIntegrationTest {
      */
     @After
     public void tearDown() throws Exception {
+        // Only for this test suite.
         if (fin.exists()) {
             fin.delete();
         }
-
         if (fout.exists()) {
             fout.delete();
         }
@@ -65,15 +71,18 @@ public class UniqIntegrationTest {
      */
     @Test
     public void uniqUniq() throws IOException {
+        // Setup the test case.
         String input = "hello\nhello\nworld\ncat\ndog";
         FileWriter fw = new FileWriter(fin);
         fw.write(input);
         fw.close();
 
+        // Call and run the shell like this.
         setupShellWithInput(String.format("uniq %s", TEST_INPUT_FILE));
         shell.run();
         String result = getStringFromOutput();
 
+        // Do your asserts.
         String expected = "hello\nworld\ncat\ndog";
         assertEquals(expected, result);
     }
